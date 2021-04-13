26be52847088a9eb4b319262923a5f9dea3283b0
Matthew Hanson <matt.a.hanson@gmail.com>
2021-04-12 23:37:03 -0400

    simplify ItemCollection

--- pystac_client/item_collection.py
+++ pystac_client/item_collection.py
@@ -5,7 +5,7 @@ import pystac
 from pystac_client.stac_api_object import STACAPIObjectMixin
 
 
-class ItemCollection(pystac.STACObject, STACAPIObjectMixin):
+class ItemCollection(object):
     """Implementation of the `STAC API ItemCollection Fragment
     <https://github.com/radiantearth/stac-api-spec/tree/master/fragments/itemcollection>`__.
 
@@ -14,32 +14,18 @@ class ItemCollection(pystac.STACObject, STACAPIObjectMixin):
     features : list
         A list of :class:`pystac.Item` instances for this instance.
 
-    conformance : list
-        A list of conformance URIs describing the specifications that this services conforms to. Note that this is
-        not published as part of the `ItemCollection Fragment
-        <https://github.com/radiantearth/stac-api-spec/blob/master/fragments/itemcollection/README.md>`__ itself and
-        must be passed to one of the constructor methods.
-
-    stac_extensions : list
-        A list of STAC extensions that the instance implements. These apply to `static extensions
-        <https://github.com/radiantearth/stac-spec/tree/master/extensions>`__ only and not
-        `API extensions <https://github.com/radiantearth/stac-api-spec/blob/master/extensions.md>`__ as described in
-        the `ItemCollection Fragment: Item Collection fields
-        <https://github.com/radiantearth/stac-api-spec/blob/master/fragments/itemcollection/README.md#itemcollection-fields>`__
-        docs.
-
-
     """
-    def __init__(self, features=None, stac_extensions=None, conformance=None, extra_fields=None):
-        super().__init__(stac_extensions)
+    def __init__(self, features=None):
+        super().__init__()
 
         features = features or []
         self.features = [f.clone() for f in features]
-        self.conformance = conformance or []
-        self.extra_fields = extra_fields or {}
+        self.links = []
+        for f in self.features:
+            f.clear_links('root')
 
-    def _object_links(self):
-        return []
+    def __getitem__(self, key):
+        return self.features[key]
 
     def to_dict(self, include_self_link=True):
         """Serializes an :class:`ItemCollection` instance to a JSON-like dictionary. """
@@ -50,14 +36,9 @@ class ItemCollection(pystac.STACObject, STACAPIObjectMixin):
 
         d = {
             'type': 'FeatureCollection',
-            'stac_version': pystac.get_stac_version(),
             'links': [link.to_dict() for link in links],
-            'features': [f.to_dict() for f in self.features],
-            **deepcopy(self.extra_fields)
+            'features': [f.to_dict() for f in self.features]
         }
-
-        if self.stac_extensions is not None:
-            d['stac_extensions'] = self.stac_extensions
 
         return d
 
@@ -79,18 +60,17 @@ class ItemCollection(pystac.STACObject, STACAPIObjectMixin):
         their own conformance URIs in a "conformsTo" attribute. To add conformance URIs to indicate which API
         extensions an :class:`ItemCollection` instance implements you MUST pass this list in using the ``conformance``
         argument. See the :meth:`API.search <pystac_client.API.search>` method for an example."""
-        d = deepcopy(d)
-
         features = [pystac.Item.from_dict(feature) for feature in d.pop('features', [])]
         links = d.pop('links', [])
-        stac_extensions = d.pop('stac_extensions', None)
 
-        item_collection = cls(features=features,
-                              stac_extensions=stac_extensions,
-                              extra_fields=deepcopy(d),
-                              conformance=conformance)
+        item_collection = cls(features=features)
 
         for link in links:
             item_collection.add_link(pystac.Link.from_dict(link))
 
         return item_collection
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename) as f: 
+            return cls.from_dict(json.loads(f.read()))
