accf0587f101d2a363134f4ad73847db0d3f1c08
Matthew Hanson <matt.a.hanson@gmail.com>
2021-06-05 01:14:45 -0400

    remove extensions implementation that were based on old PySTAC extensions

--- pystac_client/__init__.py
+++ pystac_client/__init__.py
@@ -4,14 +4,6 @@ import pystac.extensions.base
 from pystac_client.version import __version__
 from pystac_client.stac_api_object import STACAPIObjectMixin
 from pystac_client.item_collection import ItemCollection
-from pystac_client.extensions import APIExtensions
 from pystac_client.item_search import ItemSearch
 from pystac_client.client import Client
 from pystac_client.conformance import ConformanceClasses
-
-from pystac_client import extensions
-import pystac_client.extensions.context
-
-# Add API Extensions
-STAC_API_EXTENSIONS = pystac.extensions.base.RegisteredSTACExtensions(
-    [extensions.context.CONTEXT_EXTENSION_DEFINITION])
--- tests/test_stac_io.py
+++ tests/test_stac_io.py
@@ -3,7 +3,7 @@ from urllib.request import Request
 
 import pytest
 
-from pystac_client.stac_io import read_text_method
+from pystac_client.stac_io import StacApiIO
 
 from .helpers import ASTRAEA_URL, MLHUB_URL
 
@@ -11,31 +11,35 @@ from .helpers import ASTRAEA_URL, MLHUB_URL
 class TestSTAC_IOOverride:
     @pytest.mark.vcr
     def test_request_input(self):
+        stac_api_io = StacApiIO()
         request = Request(url=ASTRAEA_URL)
-        response = read_text_method(request)
+        response = stac_api_io.read_text(request)
 
         assert isinstance(response, str)
 
     @pytest.mark.vcr
     def test_str_input(self):
-        response = read_text_method(ASTRAEA_URL)
+        stac_api_io = StacApiIO()
+        response = stac_api_io.read_text(ASTRAEA_URL)
 
         assert isinstance(response, str)
 
     @pytest.mark.vcr
     def test_http_error(self):
+        stac_api_io = StacApiIO()
         # Attempt to access an authenticated endpoint
         request = Request(url=f'{MLHUB_URL}/search')
         with pytest.raises(HTTPError) as excinfo:
-            read_text_method(request)
+            stac_api_io.read_text(request)
 
         assert isinstance(excinfo.value, HTTPError)
 
     def test_local_file(self, tmp_path):
+        stac_api_io = StacApiIO()
         test_file = tmp_path / 'test.txt'
         with test_file.open('w') as dst:
             dst.write('Hi there!')
 
-        response = read_text_method(str(test_file))
+        response = stac_api_io.read_text(str(test_file))
 
         assert response == 'Hi there!'
