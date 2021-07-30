192c416cb4c79d3f066dbebaa677ea252fc36c93
Matthew Hanson <matt.a.hanson@gmail.com>
2021-07-30 00:27:56 -0400

    fix CollectionClient from_dict (pystac bug 589), redefine get_items to use items endpoint if conforming

--- pystac_client/collection_client.py
+++ pystac_client/collection_client.py
@@ -13,6 +13,7 @@ from typing import (
 
 import pystac
 from pystac_client.conformance import ConformanceClasses
+from pystac_client.item_search import ItemSearch
 
 if TYPE_CHECKING:
     from pystac.item import Item as Item_Type
@@ -23,6 +24,18 @@ class CollectionClient(pystac.Collection):
     def __repr__(self):
         return '<CollectionClient id={}>'.format(self.id)
 
-    #def get_items(self) -> Iterable["Item_Type"]:
-    #    if self.conforms_to(ConformanceClasses.COLLECTIONS):
+    def conforms_to(self, conformance: ConformanceClasses) -> bool:
+        return self._stac_io.conforms_to(conformance)
+
+    @classmethod
+    def from_dict(cls, *args, root: Optional["Catalog_Type"] = None, **kwargs):
+        col = super().from_dict(*args, **kwargs)
+        if root is not None:
+            col._stac_io = root._stac_io
+        return col
+
+    def get_items(self) -> Iterable["Item_Type"]:
+        if self.conforms_to(ConformanceClasses.COLLECTIONS):
+            url = f"{self.get_root_link()}/collections/{self.id}/items"
+            breakpoint()
 
