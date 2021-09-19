ae8ce0a04c4b872b73f37795a983723f598425a0
Matthew Hanson <matt.a.hanson@gmail.com>
2021-09-19 19:40:31 -0400

    fix regex conformance checking

--- pystac_client/conformance.py
+++ pystac_client/conformance.py
@@ -29,20 +29,22 @@ True
 """
 from enum import Enum
 
+import re
+
 
 class ConformanceClasses(Enum):
 
-    stac_prefix = r"\Qhttps://api.stacspec.org/v1.0.\E(.*)"
+    stac_prefix = re.escape("https://api.stacspec.org/v1.0.")
 
     # defined conformance classes regexes
-    CORE = rf"{stac_prefix}(.*)\Qcore\E"
-    ITEM_SEARCH = rf"{stac_prefix}(.*)\Qitem-search\E"
-    CONTEXT = rf"{stac_prefix}(.*)\Qitem-search#context\E"
-    FIELDS = rf"{stac_prefix}(.*)\Qitem-search#fields\E"
-    SORT = rf"{stac_prefix}(.*)\Qitem-search#sort\E"
-    QUERY = rf"{stac_prefix}(.*)\Qitem-search#query\E"
-    FILTER = rf"{stac_prefix}(.*)\Qitem-search#filter\E"
-    COLLECTIONS = r"\Qhttp://www.opengis.net/spec/ogcapi-features-1/1.0/conf/oas30\E"
+    CORE = fr"{stac_prefix}(.*){re.escape('/core')}"
+    ITEM_SEARCH = fr"{stac_prefix}(.*){re.escape('/item-search')}"
+    CONTEXT = fr"{stac_prefix}(.*){re.escape('/item-search#context')}"
+    FIELDS = fr"{stac_prefix}(.*){re.escape('/item-search#fields')}"
+    SORT = fr"{stac_prefix}(.*){re.escape('/item-search#sort')}"
+    QUERY = fr"{stac_prefix}(.*){re.escape('/item-search#query')}"
+    FILTER = fr"{stac_prefix}(.*){re.escape('/item-search#filter')}"
+    COLLECTIONS = re.escape("http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/oas30")
 
 
 CONFORMANCE_URIS = {c.name: c.value for c in ConformanceClasses}
