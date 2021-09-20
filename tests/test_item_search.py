d3fa1bfe72f93de42c9aa2c76d995bca8e8007c5
Matthew Hanson <matt.a.hanson@gmail.com>
2021-09-19 21:55:08 -0400

    lint and format

--- tests/test_item_search.py
+++ tests/test_item_search.py
@@ -370,26 +370,21 @@ class TestItemSearch:
 
 
 class TestItemSearchQuery:
-
     def test_query_shortcut_syntax(self):
-        search = ItemSearch(
-            url=SEARCH_URL,
-            bbox=(-73.21, 43.99, -73.12, 44.05),
-            query=["gsd=10"],
-            max_items=1
-        )
+        search = ItemSearch(url=SEARCH_URL,
+                            bbox=(-73.21, 43.99, -73.12, 44.05),
+                            query=["gsd=10"],
+                            max_items=1)
         items1 = list(search.get_items())
 
-        search = ItemSearch(
-            url=SEARCH_URL,
-            bbox=(-73.21, 43.99, -73.12, 44.05),
-            query={
-                "gsd": {"eq": 10}
-            },
-            max_items=1
-        )
+        search = ItemSearch(url=SEARCH_URL,
+                            bbox=(-73.21, 43.99, -73.12, 44.05),
+                            query={"gsd": {
+                                "eq": 10
+                            }},
+                            max_items=1)
         items2 = list(search.get_items())
 
-        assert(len(items1) == 1)
-        assert(len(items2) == 1)
-        assert(items1[0].id == items2[0].id)
+        assert (len(items1) == 1)
+        assert (len(items2) == 1)
+        assert (items1[0].id == items2[0].id)
