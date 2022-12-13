af828106c0a89fc7560288ef1d6e79a306a5b82a
Pete Gadomski <pete.gadomski@gmail.com>
2022-12-13 12:34:09 -0700

    feat: fall back on item search

--- tests/test_collection_client.py
+++ tests/test_collection_client.py
@@ -35,3 +35,22 @@ class TestCollectionClient:
 
         item = collection.get_item("for-sure-not-a-real-id")
         assert item is None
+
+    @pytest.mark.vcr
+    def test_get_item_with_item_search(self) -> None:
+        client = Client.open(STAC_URLS["PLANETARY-COMPUTER"])
+        collection = client.get_collection("aster-l1t")
+        assert collection is not None
+        assert client._stac_io
+        client._stac_io.set_conformance(
+            [
+                "https://api.stacspec.org/v1.0.0-rc.2/core",
+                "https://api.stacspec.org/v1.0.0-rc.2/item-search",
+            ]
+        )
+        item = collection.get_item("AST_L1T_00312272006020322_20150518201805")
+        assert item
+        assert item.id == "AST_L1T_00312272006020322_20150518201805"
+
+        item = collection.get_item("for-sure-not-a-real-id")
+        assert item is None
