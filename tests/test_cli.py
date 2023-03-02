2a2f9dcac4e69935777f1b93fa2a5a7151a9c057
Ian Cooke <icooke@element84.com>
2023-03-02 13:48:55 -0500

    fix(cli): header parsing when multiple equals present

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,9 @@
+from typing import List
+
 import pytest
 from pytest_console_scripts import ScriptRunner
 
+import pystac_client.cli
 from tests.helpers import STAC_URLS
 
 
@@ -18,6 +21,30 @@ class TestCLI:
         ]
         result = script_runner.run(*args, print_result=False)
         assert result.success
+
+    @pytest.mark.parametrize(
+        "headers,good_header_count",
+        [
+            (["kick=flip", "home=run"], 2),
+            (["mad=pow"], 1),
+            (["=no-var"], 0),
+            (["no-val="], 0),
+            (["good=header", "bad-header"], 1),
+            (["header=value-with-three-=-signs-=", "plain=jane"], 2),
+        ],
+    )
+    def test_headers(self, headers: List[str], good_header_count: int) -> None:
+        args = [
+            "search",
+            STAC_URLS["PLANETARY-COMPUTER"],
+            "-c",
+            "naip",
+            "--max-items",
+            "20",
+            "--headers",
+        ] + headers
+        pargs = pystac_client.cli.parse_args(args)
+        assert len(pargs["headers"]) == good_header_count
 
     def test_no_arguments(self, script_runner: ScriptRunner) -> None:
         args = ["stac-client"]
