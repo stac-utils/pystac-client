2251cb07f38836456b30308c6e1fc50572698f29
Julia Signell <jsignell@gmail.com>
2023-10-30 15:20:05 -0400

    Resolve warning in `pydata_sphinx_theme`

--- docs/conf.py
+++ docs/conf.py
@@ -103,6 +103,7 @@ html_theme_options = {
     "external_links": [
         {"name": "STAC Spec", "url": "https://github.com/radiantearth/stac-spec"}
     ],
+    "navigation_with_keys": False,
     # "navbar_end": ["navbar-icon-links.html", "search-field.html"]
 }
 
